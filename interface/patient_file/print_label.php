7a09e22b7cfbce2c63c1b00bc95c0e2030cd5930
Dennison Williams <dennison.williams@gmail.com>
2019-09-10 16:42:41 -0700

    This adds our custom label printer functionality, there is still an issue when enter is pressed though

---
+++ interface/patient_file/print_label.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * LICENSE: This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://opensource.org/licenses/gpl-license.php>;.
+ *
+ * @package OpenEMR
+ * @author  Dennison Williams <dennison.williams@stjamesinfirmary.org>
+ */
+
+use OpenEMR\Core\Header;
+
+require_once('../globals.php');
+require_once($GLOBALS['srcdir'].'/log.inc');
+require_once($GLOBALS['srcdir'].'/acl.inc');
+require_once($GLOBALS['srcdir'].'/sl_eob.inc.php');
+
+ $encounterid = $_REQUEST['encounterid'];
+
+ $info_msg = "";
+
+?>
+<html>
+<head>
+    <?php Header::setupHeader('opener'); ?>
+<title><?php echo xlt('Print visit labels'); ?></title>
+
+<script language="javascript">
+	// Java script function for closing the popup
+	function popup_close() {
+	    dlgclose();
+	}
+
+	function print_labels() {
+            console.log('print_labels()');
+	    // TODO: what does this do?
+	    top.restoreSession();
+	    // Not Empty
+	    // Strong if required
+	    // Matches
+
+	    $.post("print_label_ajax.php", {
+		    num:    $("input[name='number']").val(),
+		    encounter:    <?php echo attr($encounterid); ?>,
+		},
+		function(data) {
+		    $("#display_msg").html(data);
+		}
+
+	    );
+	    return false;
+	}
+</script>
+</head>
+
+<body class="body_top">
+<?php
+if (array_key_exists('form_submit', $_POST)) {
+    error_log(__FILE__ .": form submitted");
+    if ($encounterid) {
+
+        if (!acl_check('encounters', 'coding')) {
+            die("Not authorized!");
+        }
+
+    } else {
+        die("Nothing was recieved to print!");
+    }
+
+    if (! $info_msg) {
+        $info_msg = xl('labels printed.');
+    }
+
+  // Close this window and tell our opener that it's done.
+  // Not sure yet if the callback can be used universally.
+    echo "<script language='JavaScript'>\n";
+    if (!$encounterid) {
+        if ($info_msg) {
+            echo " alert('" . addslashes($info_msg) . "');\n";
+        }
+        echo " dlgclose('imdeleted',false);\n";
+    } else {
+        if ($GLOBALS['sql_string_no_show_screen']) {
+            echo " dlgclose('imdeleted', $encounterid);\n";
+        } else { // this allows dialog to stay open then close with button or X.
+            echo " opener.dlgSetCallBack('imdeleted', $encounterid);\n";
+        }
+    }
+    echo "</script></body></html>\n";
+    exit();
+}
+?>
+
+<div id="display_msg"></div>
+<form method='post' name="print_label_form" action='print_label.php?encounterid=<?php echo attr($encounterid) ?>' onsubmit="print_labels();">
+
+<p class="lead">&nbsp;<br>
+
+<?php echo xlt('How many?'); ?>
+<input type="text" name="number" maxlength=3 autofocus>
+<div class="btn-group">
+    <a href="#" onclick="print_labels();" class="btn btn-lg btn-save btn-default"><?php echo xlt('Print labels'); ?></a>
+    <a href='#' class="btn btn-lg btn-link btn-cancel" onclick="popup_close();"><?php echo xlt('Cancel');?></a>
+</div>
+<input type='hidden' name='form_submit' value='<?php echo xla('Yes, Print labels'); ?>'/>
+</form>
+</body>
+</html>
---
+++ interface/patient_file/print_label_ajax.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ * Controller to handle label print requests
+ *
+ * <pre>
+ * Expected REQUEST parameters
+ * $_REQUEST['num'] - The numbe rof labels to print
+ * $_REQUEST['encounter'] - The encounter we are printing the labels for
+ * </pre>
+ *
+ * @package   OpenEMR
+ * @link      http://www.open-emr.org
+ * @author    Dennison Williams <dennison.williams@gmail.com>
+ */
+
+
+require_once("../globals.php");
+require_once("$srcdir/authentication/password_change.php");
+
+$num=intval($_REQUEST['num']);
+$encounter=$_REQUEST['encounter'];
+
+error_log(__FILE__ .": num=$num");
+if (!is_int($num) || $num>100 || $num<1) {
+    echo "<div class='alert alert-danger'>" . xlt("Invalid number of labels") . "</div>";
+    exit;
+}
+
+$host = '192.168.20.246';
+$port = 9100;
+$errMsg='';
+
+// TODO:
+// Get chart number, visit date, name, DOB, Sex assigned at birth, and HAP #
+$sql = 'SELECT date,pid FROM form_encounter WHERE encounter=?';
+$res = sqlStatement($sql, array($encounter));
+$row = sqlFetchArray($res);
+$date = $row['date'];
+$pid = $row['pid'];
+
+$sql = 'SELECT DOB,sex,fname,lname FROM patient_data WHERE pid=?';
+$res = sqlStatement($sql, array($pid));
+$row = sqlFetchArray($res);
+$dob = $row['DOB'];
+$sex = $row['sex'];
+if (isset($row['lname'])) {
+   $name = $row['lname'] .', ';
+}
+$name .= $row['fname'];
+
+$sql = 'SELECT gender FROM form_sji_intake_core_variables WHERE pid=?';
+$res = sqlStatement($sql, array($pid));
+$row = sqlFetchArray($res);
+$gender = $row['gender'];
+$sex_code = code_sex($sex, $gender);
+
+$sql = 'SELECT name,policy_number FROM insurance_data '.
+       'LEFT JOIN insurance_companies on (insurance_data.provider=insurance_companies.id) '.
+       'AND pid=?';
+$res = sqlStatement($sql, array($pid));
+$ins = '';
+while ( $row = sqlFetchArray($res) ) {
+   $ins .= '^FD'. $row['name'] .': '. $row['policy_number'] ."^FS\n";
+}
+
+error_log(__FILE__ .': date='. $date .", pid=". $pid 
+    .", dob=". $dob .", gender: $gender, sex: $sex, sex code: $sex_code, insurance:\n$ins");
+
+// Create label string
+$message = <<<MSG
+^XA
+^LH45,20
+^PQ$num
+^CFF
+^FO15,00
+^FDSJI/CTYC: $pid^FS
+^CFD
+^FO00,30
+^FDVisit: $date^FS
+^FO00,55
+^FD$name^FS
+^FO00,80
+^FDDOB: $dob $sex_code^FS
+^FO00,105
+$ins
+^PH
+^XZ
+MSG;
+
+error_log(__FILE__ .": message=\n$message");
+
+// create socket
+$success = true;
+$socket = socket_create(AF_INET, SOCK_STREAM, 0);
+if (!$socket) {
+   $success = false;
+   $errMsg = 'Could not connect to label printer';
+   error_log(__FILE__ .': socket_create() '. socket_strerror(socket_last_error()));
+} 
+
+$result = false;
+if ($socket) {
+   // connect to server
+   $result = socket_connect($socket, $host, $port);
+}
+if (!$result) {
+   $success = false;
+   $errMsg = 'Could not connect to label printer';
+   error_log(__FILE__ .': socket_connect() '. socket_strerror(socket_last_error()));
+}
+
+if ($result) {
+   // send string to server
+   $sent = socket_write($socket, $message, strlen($message));
+   if ($sent === false) {
+      $success = false;
+      $errMsg = 'Could not connect to label printer';
+      error_log(__FILE__ .': socket_write() '. socket_strerror(socket_last_error()));
+   }
+}
+
+if ($socket) {
+   // close socket
+   socket_close($socket);
+}
+
+if ($success) {
+    echo "<div class='alert alert-success'>$num " . 
+       xlt("labels sent to printer") . "</div>";
+} else {
+    // If update_password fails the error message is returned
+    echo "<div class='alert alert-danger'>" . text($errMsg) . "</div>";
+}
+
+/*
+if sex == 'A': sex = 'TF'
+if sex == 'B': sex = 'TM'
+if sex == 'C': sex = 'I'
+if sex == 'CF': sex = 'IF'
+if sex == 'CM': sex = 'IM'
+if sex == 'M': sex = 'CM'
+if sex == 'F': sex = 'CF'
+if sex == 'OF': sex = 'NB AFAB'
+if sex == 'OM': sex = 'NB AMAB'
+*/
+function code_sex($sex, $gender) {
+   if(isset($sex) and !isset($gender)) {
+      if ($sex == 'Male') { return 'CM'; }
+      if ($sex == 'Female') { return 'CF'; }
+   }
+
+   if ($gender == 'Transgender Female') { return 'TF'; }
+   if ($gender == 'Cisgender Female') { return 'CF'; }
+   if ($gender == 'Cisgender Male') { return 'CM'; }
+   if ($gender == 'Transgender Male') { return 'TM'; }
+   if ($gender == 'Intersex male') { return 'IM'; }
+   if ($gender == 'Other Male') { return 'NB AMAB'; }
+   if ($gender == 'Other Female') { return 'NB AFAB'; }
+   if ($gender == 'Intersex Female') { return 'IF'; }
+}

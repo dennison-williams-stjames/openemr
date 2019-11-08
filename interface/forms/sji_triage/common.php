866d4d4ef7f32a541e982aa4933caa97938addc5
Dennison Williams <dennison.williams@gmail.com>
2019-11-07 17:16:20 -0800

    Initial incomplete work

--- interface/forms/sji_triage/common.php
+++ interface/forms/sji_triage/common.php
@@ -3,83 +3,78 @@ require_once(dirname(__FILE__).'/../../globals.php');
 include_once("$srcdir/api.inc");
 include_once("$srcdir/forms.inc");
 
-$medical_psychiatric_columns = array(
-   'duration',
-   'evaluate_manage_new',
-   'evaluate_manage_established'
+$triage_columns = array(
+   'chief_complaint',
+   'notes',
+   'concerns',
+   'services'
 );
 
-function sji_extendedMedicalPsychiatric_formFetch($formid) {
+function sji_extendedTriage_formFetch($formid) {
+    global $pid;
+    $encounter = $_SESSION['encounter'];
     $return = array();
 
+    // Get options from form_vitals for user: temperature, sistolic, distolic
     $res = sqlStatement(
-       "select provider_type ".
-       "from form_sji_medical_psychiatric_provider_type ".
-       "where pid=?", array($formid));
+       "select temperature,bps,bpd ".
+       "from form_vitals ".
+       "where pid=? ".
+       "order by id asc limit 1", array($pid));
 
     while ($row = sqlFetchArray($res)) {
-       $return['provider_type'][] = $row['provider_type'];
+       $return['temperature'] = $row['temperature'];
+       $return['bps'] = $row['bps'];
+       $return['bpd'] = $row['bpd'];
     }
 
+    // get participant information
     $res = sqlStatement(
-       "select icd_primary ".
-       "from form_sji_medical_psychiatric_icd9_primary ".
-       "where pid=?", array($formid));
+       "select fname, lname, email, hipaa_allowemail, hippa_allowsms, hippa_message, hippa_voice ".
+       "from patient_data ".
+       "where pid=?", array($pid));
 
     while ($row = sqlFetchArray($res)) {
-       $return['icd9_primary'][] = $row['icd_primary'];
+       $return['name'] = $row['fname'] ." ". $row['lname'];
+       $return['email'] = $row['email'];
+       $return['hipaa_allowemail'] = $row['hipaa_allowemail'];
+       $return['hipaa_allowsms'] = $row['hipaa_allowsms'];
+       $return['hipaa_message'] = $row['hipaa_message'];
+       $return['hipaa_voice'] = $row['hipaa_voice'];
     }
 
+    // get a few items from our core variables
     $res = sqlStatement(
-       "select icd_secondary ".
-       "from form_sji_medical_psychiatric_icd9_secondary ".
-       "where pid=?", array($formid));
+       "select gender, pronouns, aliases ".
+       "from form_sji_intake_core_variables ".
+       "where pid=? order by id asc limit 1", array($pid));
 
     while ($row = sqlFetchArray($res)) {
-       $return['icd9_secondary'][] = $row['icd_secondary'];
-    }
-
-    $res = sqlStatement(
-       "select cpt_codes ".
-       "from form_sji_medical_psychiatric_cpt_codes ".
-       "where pid=?", array($formid));
-
-    while ($row = sqlFetchArray($res)) {
-       $return['cpt_codes'][] = $row['cpt_codes'];
-    }
-
-    $res = sqlStatement(
-       "select range_codes ".
-       "from form_sji_medical_psychiatric_range_codes ".
-       "where pid=?", array($formid));
-
-    while ($row = sqlFetchArray($res)) {
-       $return['range_codes'][] = $row['range_codes'];
-    }
-
-    $res = sqlStatement(
-       "select method_codes ".
-       "from form_sji_medical_psychiatric_method_codes ".
-       "where pid=?", array($formid));
-
-    while ($row = sqlFetchArray($res)) {
-       $return['methods_codes'][] = $row['method_codes'];
-    }
-
-    $res = sqlStatement(
-       "select contraception_method ".
-       "from form_sji_medical_psychiatric_contraception_method ".
-       "where pid=?", array($formid));
-
-    while ($row = sqlFetchArray($res)) {
-       $return['contraception_method'][] = $row['contraception_method'];
+       $return['gender'] = $row['gender'];
+       $return['pronouns'] = $row['pronouns'];
+       $return['aliases'] = $row['aliases'];
     }
 
     return $return;
 }
 
-function sji_extendedMedicalPsychiatric($formid, $submission) {
+function sji_extendedTriage($formid, $submission) {
     global $pid;
+    $encounter = $_SESSION['encounter'];
+
+    // If we were passed in vitals we need to calculate a few values and either
+    // update an existing vitals record, or add a new one
+    if (!empty($submission['temperature']) || !empty($submission['bps']) || !empty($submission['bpd'])) {
+       $sql = "select form_id from forms where pid=? and encounter=? and form_name='Vitals' order by id desc limit 1";
+       $row = sqlFetchArray(sqlStatement($sql, array($pid, $encounter)));
+       if (!empty($row)) {
+          $sql = "insert into form_vitals(temperature, bps, bpd, pid, temp_method) values(?, ?, ?, $pid, 'Oral')";
+          // TODO: error checking
+          sqlStatement($sql, array($submission['temperature'], $submission['bps'], $submission['bpd']));
+       } else {
+          // TODO: add new vital forms
+       }
+    }
 
     sqlStatement("delete from form_sji_medical_psychiatric_provider_type where pid=?", array($formid));
     if (isset($submission['provider_type'])) {
--- interface/forms/sji_triage/table.sql
+++ interface/forms/sji_triage/table.sql
@@ -1,53 +1,4 @@
-CREATE TABLE IF NOT EXISTS `form_sji_medical_psychiatric_icd9_primary` (
-        id bigint(20) NOT NULL auto_increment,
-        pid bigint(20) default NULL,
-        `icd_primary` varchar(200) NOT NULL,
-        PRIMARY KEY (id)
-) ENGINE=InnoDB;
-
-CREATE TABLE IF NOT EXISTS `form_sji_medical_psychiatric_icd9_secondary` (
-        id bigint(20) NOT NULL auto_increment,
-        pid bigint(20) default NULL,
-        `icd_secondary` varchar(200) NOT NULL,
-        PRIMARY KEY (id)
-) ENGINE=InnoDB;
-
-CREATE TABLE IF NOT EXISTS `form_sji_medical_psychiatric_cpt_codes` (
-        id bigint(20) NOT NULL auto_increment,
-        pid bigint(20) default NULL,
-        `cpt_codes` varchar(200) NOT NULL,
-        PRIMARY KEY (id)
-) ENGINE=InnoDB;
-
-CREATE TABLE IF NOT EXISTS `form_sji_medical_psychiatric_method_codes` (
-        id bigint(20) NOT NULL auto_increment,
-        pid bigint(20) default NULL,
-        `method_codes` varchar(200) NOT NULL,
-        PRIMARY KEY (id)
-) ENGINE=InnoDB;
-
-CREATE TABLE IF NOT EXISTS `form_sji_medical_psychiatric_range_codes` (
-        id bigint(20) NOT NULL auto_increment,
-        pid bigint(20) default NULL,
-        `range_codes` varchar(200) NOT NULL,
-        PRIMARY KEY (id)
-) ENGINE=InnoDB;
-
-CREATE TABLE IF NOT EXISTS `form_sji_medical_psychiatric_contraception_method` (
-        id bigint(20) NOT NULL auto_increment,
-        pid bigint(20) default NULL,
-        `contraception_method` varchar(200) NOT NULL,
-        PRIMARY KEY (id)
-) ENGINE=InnoDB;
-
-CREATE TABLE IF NOT EXISTS `form_sji_medical_psychiatric_provider_type` (
-        id bigint(20) NOT NULL auto_increment,
-        pid bigint(20) default NULL,
-        `provider_type` varchar(255) NOT NULL,
-        PRIMARY KEY (id)
-) ENGINE=InnoDB;
-
-CREATE TABLE IF NOT EXISTS `form_sji_medical_psychiatric` (
+CREATE TABLE IF NOT EXISTS `form_sji_triage` (
 	id bigint(20) NOT NULL auto_increment,
 	date datetime default NULL,
 	pid bigint(20) default NULL,
@@ -56,48 +7,11 @@ CREATE TABLE IF NOT EXISTS `form_sji_medical_psychiatric` (
 	authorized tinyint(4) default NULL,
 	activity tinyint(4) default NULL,
 
-	provider_type varchar(255),
-	evaluate_manage_new tinyint(1),
-	evaluate_manage_established tinyint(1),
-	duration varchar(255),
+	chief_complaint varchar(255),
+	notes varchar(255),
+	concerns varchar(255),
+	services varchar(255),
 
 	PRIMARY KEY (id)
 ) ENGINE=InnoDB;
-
--- generate default list of medical\psychiatric provider types --
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_provider_type', 'Counselor', 'Counselor');
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_provider_type', 'MD', 'MD');
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_provider_type', 'MD Pysch', 'MD Pysch');
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_provider_type', 'MFT/LCSW/PYSCH', 'MFT/LCSM/PYSCH');
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_provider_type', 'PA', 'PA');
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_provider_type', 'RN', 'RN');
-
--- There is no durault duration list in OpenEMR.  Even though we will use these fields --
--- in other forms, we will assign them their own context --
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_duration', '15 minutes', '15 minutes');
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_duration', '30 minutes', '30 minutes');
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_duration', '45 minutes', '45 minutes');
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_duration', '60 minutes', '60 minutes');
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_duration', '90 minutes', '90 minutes');
-
--- Import the different contraception methods --
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_contraception_method', 'Abstinence', 'Abstinence');
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_contraception_method', 'Diaphragm/Cervical Cap', 'Diaphragm/Cervical Cap');
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_contraception_method', 'Female Condom', 'Female Condom');
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_contraception_method', 'Female Sterilization', 'Female Sterilization');
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_contraception_method', 'Fertility Awareness', 'Fertility Awareness');
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_contraception_method', 'Hormonal Implant', 'Hormonal Implant');
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_contraception_method', 'Hormonal Injections 3-Month', 'Hormonal Injections 3-Month');
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_contraception_method', 'IUD', 'IUD');
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_contraception_method', 'Male Condoms', 'Male Condoms');
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_contraception_method', 'Oral Contraception', 'Oral Contraception');
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_contraception_method', 'Pregnant/Partner Pregnant', 'Pregnant/Partner Pregnant');
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_contraception_method', 'Relay on Female', 'Relay on Female');
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_contraception_method', 'Seeking Pregnancy', 'Seeking Pregnancy');
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_contraception_method', 'Spermicide Alone', 'Spermicide Alone');
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_contraception_method', 'Sponge', 'Sponge');
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_contraception_method', 'Vaginal Ring', 'Vaginal Ring');
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_contraception_method', 'Vasectomy', 'Vasectomy');
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_contraception_method', 'Other Method (Withdrawal)', 'Other Method (Withdrawal)');
-INSERT INTO list_options(list_id, option_id, title) values('sji_medical_psychiatric_contraception_method', 'No Method - Other Reason - Same Sex Partner', 'No Method - Other Reason - Same Sex Partner');
 

c04c384290376d2fdf240d2db5116c64ee2aefa1
Dennison Williams <dennison.williams@gmail.com>
2020-01-23 13:17:12 -0800

    Updates to the form structure from a talk with Carry

--- interface/forms/sji_counseling/table.sql
+++ interface/forms/sji_counseling/table.sql
@@ -1,33 +1,42 @@
+CREATE TABLE IF NOT EXISTS `form_sji_counseling_counseling` (
+	id bigint(20) NOT NULL auto_increment,
+	`counseling` varchar(200) NOT NULL,
+	pid bigint(20) default NULL,
+	PRIMARY KEY (id),
+        KEY `pid` (`pid`),
+        CONSTRAINT `form_sji_counseling_counseling_ibfk_1` FOREIGN KEY (`pid`) REFERENCES `form_sji_counseling` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
 CREATE TABLE IF NOT EXISTS `form_sji_counseling` (
-id bigint(20) NOT NULL auto_increment,
-date datetime default NULL,
-pid bigint(20) default NULL,
-user varchar(255) default NULL,
-groupname varchar(255) default NULL,
-authorized tinyint(4) default NULL,
-activity tinyint(4) default NULL,
+	id bigint(20) NOT NULL auto_increment,
+	date datetime default NULL,
+	pid bigint(20) default NULL,
+	user varchar(255) default NULL,
+	groupname varchar(255) default NULL,
+	authorized tinyint(4) default NULL,
+	activity tinyint(4) default NULL,
 
-counseling_type varchar(255),
-counseling_time varchar(255),
-progress_notes varchar(255),
+	counseling_type varchar(255),
+	counseling_time varchar(255),
+	progress_notes varchar(500),
 
-PRIMARY KEY (id)
+	PRIMARY KEY (id)
 ) ENGINE=InnoDB;
 
 -- generate default list of counseling services --
-INSERT INTO list_options(list_id, option_id, title) values('sji_counseling', 'Counseling/Prevent Gp', 'Counseling/Prevent Gp');
-INSERT INTO list_options(list_id, option_id, title) values('sji_counseling', 'General Prevention', 'General Prevention');
-INSERT INTO list_options(list_id, option_id, title) values('sji_counseling', 'HAP Sign-Up', 'HAP Sign-Up');
-INSERT INTO list_options(list_id, option_id, title) values('sji_counseling', 'Initial Family Plan Meth', 'Initial Family Plan Meth');
-INSERT INTO list_options(list_id, option_id, title) values('sji_counseling', 'Language Interpretation', 'Language Interpretation');
-INSERT INTO list_options(list_id, option_id, title) values('sji_counseling', 'Peer Counseling', 'Peer Counseling');
-INSERT INTO list_options(list_id, option_id, title) values('sji_counseling', 'Risk Reduction Coun', 'Risk Reduction Coun');
-INSERT INTO list_options(list_id, option_id, title) values('sji_counseling', 'Smoking Cessation', 'Smoking Cessation');
-INSERT INTO list_options(list_id, option_id, title) values('sji_counseling', 'Other Counseling', 'Other Counseling');
-INSERT INTO list_options(list_id, option_id, title) values('sji_counseling', 'New Participant Intake', 'New Participant Intake');
-INSERT INTO list_options(list_id, option_id, title) values('sji_counseling', 'Group Counseling', 'Group Counseling');
-INSERT INTO list_options(list_id, option_id, title) values('sji_counseling', 'Case Management', 'Case Management');
-INSERT INTO list_options(list_id, option_id, title) values('sji_counseling', 'Linkage to care', 'Linkage to care');
+INSERT INTO list_options(list_id, option_id, title) values('sji_session_type', 'Intial Intake', 'Initial Intake');
+INSERT INTO list_options(list_id, option_id, title) values('sji_session_type', 'Peer Counseling', 'Peer Counseling');
+INSERT INTO list_options(list_id, option_id, title) values('sji_session_type', 'STI Screening / Results', 'STI Screening / Results');
+
+-- generate default list of counseling services --
+INSERT INTO list_options(list_id, option_id, title) values('sji_counseling', 'Test results', 'Test results');
+INSERT INTO list_options(list_id, option_id, title) values('sji_counseling', 'Substance use counseling', 'Substance use counseling');
+INSERT INTO list_options(list_id, option_id, title) values('sji_counseling', 'HIV test', 'HIV test');
+INSERT INTO list_options(list_id, option_id, title) values('sji_counseling', 'Goal setting', 'Goal Setting');
+INSERT INTO list_options(list_id, option_id, title) values('sji_counseling', 'Referring partners', 'Referring partners');
+INSERT INTO list_options(list_id, option_id, title) values('sji_counseling', 'Motivational interview', 'Motivational interview');
+INSERT INTO list_options(list_id, option_id, title) values('sji_counseling', 'Risk reduction plan', 'Risk reduction plan');
+INSERT INTO list_options(list_id, option_id, title) values('sji_counseling', 'Health education', 'Health education');
 
 -- There is no durault duration list in OpenEMR.  Even though we will use these fields --
 -- in other forms, we will assign them their own context --

5469b92dfda88f5de5c41e4d92eacabc048c2c1a
Dennison Williams <Dennison.Williams@stjamesinfirmary.org>
2021-06-23 10:40:08 -0700

    Adds the scheduled provider feature to the calendar

--- interface/main/calendar/modules/PostCalendar/pnuserapi.php
+++ interface/main/calendar/modules/PostCalendar/pnuserapi.php
@@ -31,6 +31,7 @@
 //  Require utility classes
 //=========================================================================
 require_once($GLOBALS['fileroot'] . "/library/patient.inc");
+require_once($GLOBALS['fileroot']."/library/appointments.inc.php");
 require_once($GLOBALS['fileroot'] . "/library/group.inc");
 require_once($GLOBALS['fileroot'] . "/library/encounter_events.inc.php");
 $pcModInfo = pnModGetInfo(pnModGetIDFromName(__POSTCALENDAR__));
@@ -324,6 +325,28 @@ function postcalendar_userapi_buildView($args)
         // filter the display on the requested username, the provinfo array is
         // used to build columns in the week view.
 
+        // if we were passed in __PC_SCHEDULED__ then we only want a list of 
+        // providers that are currently scheduled
+        if (is_array($pc_username) && $pc_username[0] === '__PC_SCHEDULED__') {
+            $eventsByDate =& postcalendar_userapi_pcGetEvents(
+               array('start'=>$starting_date,'end'=>$ending_date, 'viewtype' => $viewtype));
+            $providers = array();
+            foreach ($eventsByDate as $date => $events) {
+               foreach ($events as $event) {
+                  if ($event['catname'] === 'In Office') {
+                     $providers[$event['aid']] = 1;
+                  } // if
+               } // foreach
+            } // foreach
+            $provIDs = array_keys($providers);
+
+            foreach ($provinfo as $provider) {
+               if (array_search($provider['id'], $provIDs) !== FALSE) {
+                  array_push($single, $provider);
+               } // if
+            } // foreach
+
+        } else {
         foreach ($provinfo as $provider) {
             if (is_array($pc_username)) {
                 foreach ($pc_username as $uname) {
@@ -338,7 +361,8 @@ function postcalendar_userapi_buildView($args)
                     array_push($provIDs, $provider['id']);
                 }
             }
-        }
+        } // foreach
+	} // else
 
         if ($single != null) {
             $provinfo = $single;

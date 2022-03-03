ec4b05b08c2d68a8ae5b1632d82da39b7b4763ee
Dennison Williams <Dennison.Williams@stjamesinfirmary.org>
2022-03-03 11:34:35 -0800

    Restoring core demographics section

--- interface/patient_file/summary/demographics.php
+++ interface/patient_file/summary/demographics.php
@@ -1936,10 +1936,5 @@ if ($track_is_registered) {
 
 </script>
 
-<script>
-    /* Custom page modifications for SJI */
-    $('div.demographics-box section:nth-child(2)').hide();
-</script>
-
 </body>
 </html>
